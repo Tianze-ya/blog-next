c20642fbb76825e2efc9b4040ccc3aed375201bf
Tianze-ya <1538058674@qq.com>
2025-08-19 18:51:17 +0800

    auto

--- src/components/LoadingAnimation.tsx
+++ src/components/LoadingAnimation.tsx
@@ -13,120 +13,206 @@ const LoadingAnimation: React.FC<LoadingAnimationProps> = ({ isVisible }) => {
         {/* 渐变定义 */}
         <svg height="0" width="0" viewBox="0 0 64 64" className="absolute">
           <defs xmlns="http://www.w3.org/2000/svg">
-            <linearGradient id="b" x1="0" y1="62" x2="0" y2="2">
+            <linearGradient
+              gradientUnits="userSpaceOnUse"
+              y2="2"
+              x2="0"
+              y1="62"
+              x1="0"
+              id="b"
+            >
               <stop stopColor="#973BED" />
               <stop stopColor="#007CFF" offset="1" />
             </linearGradient>
-
-            <linearGradient id="c" x1="0" y1="64" x2="0" y2="0">
+            <linearGradient
+              gradientUnits="userSpaceOnUse"
+              y2="0"
+              x2="0"
+              y1="64"
+              x1="0"
+              id="c"
+            >
               <stop stopColor="#FFC800" />
               <stop stopColor="#F0F" offset="1" />
               <animateTransform
-                attributeName="gradientTransform"
-                type="rotate"
-                values="0 32 32;-270 32 32;-270 32 32;-540 32 32;-540 32 32;-810 32 32;-810 32 32;-1080 32 32;-1080 32 32"
-                keyTimes="0; 0.125; 0.25; 0.375; 0.5; 0.625; 0.75; 0.875; 1"
-                dur="9s"
                 repeatCount="indefinite"
+                keySplines=".42,0,.58,1;.42,0,.58,1;.42,0,.58,1;.42,0,.58,1;.42,0,.58,1;.42,0,.58,1;.42,0,.58,1;.42,0,.58,1"
+                keyTimes="0; 0.125; 0.25; 0.375; 0.5; 0.625; 0.75; 0.875; 1"
+                dur="8s"
+                values="0 32 32;-270 32 32;-270 32 32;-540 32 32;-540 32 32;-810 32 32;-810 32 32;-1080 32 32;-1080 32 32"
+                type="rotate"
+                attributeName="gradientTransform"
               />
             </linearGradient>
-
-            <linearGradient id="d" x1="0" y1="62" x2="0" y2="2">
+            <linearGradient
+              gradientUnits="userSpaceOnUse"
+              y2="2"
+              x2="0"
+              y1="62"
+              x1="0"
+              id="d"
+            >
               <stop stopColor="#00E0ED" />
               <stop stopColor="#00DA72" offset="1" />
             </linearGradient>
-
-            <linearGradient id="e" x1="0" y1="62" x2="0" y2="2">
+            <linearGradient
+              gradientUnits="userSpaceOnUse"
+              y2="2"
+              x2="0"
+              y1="62"
+              x1="0"
+              id="e"
+            >
               <stop stopColor="#FF6B6B" />
               <stop stopColor="#4ECDC4" offset="1" />
             </linearGradient>
-
-            <linearGradient id="f" x1="0" y1="62" x2="0" y2="2">
+            <linearGradient
+              gradientUnits="userSpaceOnUse"
+              y2="2"
+              x2="0"
+              y1="62"
+              x1="0"
+              id="f"
+            >
               <stop stopColor="#A8E6CF" />
               <stop stopColor="#88D8C0" offset="1" />
             </linearGradient>
-
-            <linearGradient id="g" x1="0" y1="62" x2="0" y2="2">
+            <linearGradient
+              gradientUnits="userSpaceOnUse"
+              y2="2"
+              x2="0"
+              y1="62"
+              x1="0"
+              id="g"
+            >
               <stop stopColor="#FFD93D" />
               <stop stopColor="#FF6B6B" offset="1" />
             </linearGradient>
           </defs>
         </svg>
 
-        {/* T - 第一个字母 */}
-        <svg width="64" height="64" viewBox="0 0 64 64" className="inline-block">
+        {/* w */}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 64 64"
+          height="64"
+          width="64"
+          className="inline-block"
+        >
           <path
-            stroke="url(#b)"
-            strokeWidth="8"
-            strokeLinecap="round"
             strokeLinejoin="round"
+            strokeLinecap="round"
+            strokeWidth="8"
+            stroke="url(#b)"
             d="M 4,8 L 16,48 L 28,12 L 40,48 L 52,8"
             className="dash"
+            id="w"
             pathLength="360"
           />
         </svg>
 
-        {/* I - 第二个字母 */}
-        <svg width="64" height="64" viewBox="0 0 64 64" className="inline-block">
+        {/* i */}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 64 64"
+          height="64"
+          width="64"
+          className="inline-block"
+        >
           <path
-            stroke="url(#c)"
-            strokeWidth="8"
-            strokeLinecap="round"
             strokeLinejoin="round"
+            strokeLinecap="round"
+            strokeWidth="8"
+            stroke="url(#c)"
+            d="M 12,8 L 12,40 Q 12,52 24,52 L 40,52 Q 52,52 52,40 L 52,8"
+            className="dash"
+            id="u1"
+            pathLength="360"
+          />
+        </svg>
+
+        {/* X */}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 64 64"
+          height="64"
+          width="64"
+          className="inline-block"
+        >
+          <path
+            strokeLinejoin="round"
+            strokeLinecap="round"
+            strokeWidth="8"
+            stroke="url(#d)"
+            d="M 12,12 L 52,52 M 52,12 L 12,52"
+            className="dash"
+            id="x"
+            pathLength="360"
+          />
+        </svg>
+
+        {/* I */}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 64 64"
+          height="64"
+          width="64"
+          className="inline-block"
+        >
+          <path
+            strokeLinejoin="round"
+            strokeLinecap="round"
+            strokeWidth="8"
+            stroke="url(#e)"
             d="M 20,12 L 44,12 M 32,12 L 32,52 M 20,52 L 44,52"
             className="dash"
+            id="i"
             pathLength="360"
           />
         </svg>
 
-        {/* A - 第三个字母 */}
-        <svg width="64" height="64" viewBox="0 0 64 64" className="inline-block">
+        {/* A */}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 64 64"
+          height="64"
+          width="64"
+          className="inline-block"
+        >
           <path
-            stroke="url(#d)"
-            strokeWidth="8"
-            strokeLinecap="round"
             strokeLinejoin="round"
+            strokeLinecap="round"
+            strokeWidth="8"
+            stroke="url(#f)"
             d="M 12,52 L 32,12 L 52,52 M 20,36 L 44,36"
             className="dash"
+            id="a"
             pathLength="360"
           />
         </svg>
 
-        {/* N - 第四个字母 */}
-        <svg width="64" height="64" viewBox="0 0 64 64" className="inline-block">
+        {/* N */}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 64 64"
+          height="64"
+          width="64"
+          className="inline-block"
+        >
           <path
-            stroke="url(#e)"
-            strokeWidth="8"
-            strokeLinecap="round"
             strokeLinejoin="round"
+            strokeLinecap="round"
+            strokeWidth="8"
+            stroke="url(#g)"
             d="M 12,52 L 12,12 L 52,52 L 52,12"
             className="dash"
-            pathLength="360"
-          />
-        </svg>
-
-        {/* Z - 第五个字母 */}
-        <svg width="64" height="64" viewBox="0 0 64 64" className="inline-block">
-          <path
-            stroke="url(#f)"
-            strokeWidth="8"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M 12,12 L 52,12 L 12,52 L 52,52"
-            className="dash"
-            pathLength="360"
-          />
-        </svg>
-
-        {/* E - 第六个字母 */}
-        <svg width="64" height="64" viewBox="0 0 64 64" className="inline-block">
-          <path
-            stroke="url(#g)"
-            strokeWidth="8"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M 12,12 L 52,12 L 12,12 L 12,52 L 52,52 M 12,32 L 40,32"
-            className="dash"
+            id="n"
             pathLength="360"
           />
         </svg>
@@ -138,19 +224,31 @@ const LoadingAnimation: React.FC<LoadingAnimationProps> = ({ isVisible }) => {
           margin: 0.25em 0;
           gap: 0.2em;
         }
+
         .dash {
-          animation: 
-            dashArray 3s ease-in-out infinite,
-            dashOffset 3s linear infinite;
+          animation: dashArray 2s ease-in-out infinite,
+            dashOffset 2s linear infinite;
         }
+
         @keyframes dashArray {
-          0% { stroke-dasharray: 0 1 359 0; }
-          50% { stroke-dasharray: 0 359 1 0; }
-          100% { stroke-dasharray: 359 1 0 0; }
+          0% {
+            stroke-dasharray: 0 1 359 0;
+          }
+          50% {
+            stroke-dasharray: 0 359 1 0;
+          }
+          100% {
+            stroke-dasharray: 359 1 0 0;
+          }
         }
+
         @keyframes dashOffset {
-          0% { stroke-dashoffset: 365; }
-          100% { stroke-dashoffset: 5; }
+          0% {
+            stroke-dashoffset: 365;
+          }
+          100% {
+            stroke-dashoffset: 5;
+          }
         }
       `}</style>
     </div>
